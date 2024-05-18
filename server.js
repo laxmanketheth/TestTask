112a8a470fdc599fcedb46ef9393969c01cc4b02
Laxman <laxmanketheth76@gmail.com>
2024-05-18 16:53:25 +0800

    code formatted

--- server.js
+++ server.js
@@ -14,17 +14,23 @@ app.use(express.urlencoded({ extended: false }));
 const port = 8080;
 const API_KEY = process.env.API_KEY;
 
-
 app.listen(port, () => {
     console.log(`listening to port ${port}`);
 });
 
 
+/**
+ * Purpose: accepts req body and fetches data (based on lat,lon) and then save it to the database.
+ * Response: sends the weather data (saved in database table) as response.
+ */
 app.post('/weather', async (req, res) => {
     try {
         const { lat, lon } = req.body
 
-        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`)
+        const API_URL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`;
+
+        //1. Fetching data
+        const response = await fetch(API_URL)
         if (response) {
             const data = await response.json();
             const weather_data = {
@@ -39,33 +45,44 @@ app.post('/weather', async (req, res) => {
                 lat: data.coord.lat,
                 lon: data.coord.lon
             }
-
+            //2. Saving to database
             let responsedata = await db
                 .insert(weather_data, ['*'])
                 .into('weather_data');
             res.status(200).json({ success: true, data: responsedata })
         }
-        else (res.status(401).json('failed to fetch data'))
+        else (res.status(401).json({ err: 'failed to fetch' }))
     }
 
     catch (err) {
+        //Error response
         res.status(501).json({ success: false, error: err.toString() })
     }
 });
 
 
+/**
+ * Purpose: To get weather data from the database based on the lat, lon.
+ * Query Params: lat - latitude (float)
+ *               lon -  longitude(float)
+ * Response(res): retrieves data from the database based on query params
+ */
 app.get('/weatherdata', async (req, res) => {
     try {
         const lat = req.query.lat;
         const lon = req.query.lon;
+
+        //1. Get data from db
         let data = await db.select('*')
             .from('weather_data')
             .where('lat', '=', lat)
             .andWhere('lon', '=', lon);
+
+        //2. Response
         res.status(200).json({ success: true, data: data });
 
     } catch (err) {
-        // res.status(501).json({ success: false, data: { error: err.toString() } })
+        //Error response
         res.status(501).json({ success: false, error: err.toString() })
     }
 });
